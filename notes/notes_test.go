d6eeafc1ab187532f409d24ad740d0eb259caad0
go-deepak-go <mr.deepackbarwal@gmail.com>
2026-06-13 18:27:19 +0530

    add: tests for GetNote

--- notes/notes_test.go
+++ notes/notes_test.go
@@ -43,3 +43,31 @@ func getTestStore() notes.Store {
 	}
 }
 
+func TestGetNote_FindsNoteInStoreByID(t *testing.T) {
+	t.Parallel()
+	store := getTestStore()
+	want := notes.Note{
+		ID: "1",
+		Title: "Go maps",
+		Body: "Maps are great.",
+		Notebook: "",
+		Tags: []string{"go", "learning"},
+		Pinned: true,
+	}
+	got, ok := store.GetNote("1")
+	if !ok {
+		t.Fatal("Note not found!")
+	}
+	if want != got {
+		t.Fatalf("want %#v, got %#v", want, got)
+	}
+}
+
+func TestGetNote_ReturnsFalseWhenNoteNotFound(t *testing.T) {
+	t.Parallel()
+	store = getTestStore()
+	_, ok := store.GetNote("nonexistent ID")
+	if ok {
+		t.Fatal("want false for nonexistent ID, got true")
+	}
+}
